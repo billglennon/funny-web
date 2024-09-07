7f2e9360e35e460f38a64325ee7632676533ba7b
Bill Glennon <1130086+billglennon@users.noreply.github.com>
2024-09-07 17:33:12 -0400

    Now with logging!

--- app.py
+++ app.py
@@ -8,5 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def tell_a_joke():
+    print("Beginning to tell a joke!")
     joke = random.choice(jokes)
+    print(f"The joke we are telling is {joke}")
     return flask.render_template('joke.html', joke_text=joke)
